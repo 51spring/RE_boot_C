e99c08b67a69471235fbd3ee2aa34477a72da875
seungkyumas <sklee3504@naver.com>
2026-04-05 15:23:30 +0900

    Update 2.c

--- week1/problems/lsg/2.c
+++ week1/problems/lsg/2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+	int a;
+	scanf_s("%d", &a);
+
+	for (int i = 1; i <= a; i++) 
+	{
+		for (int j = 0; j < a - i; j++) 
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < 2 * i - 1; j++) 
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
